10760ce339372bfb874085b8931c8c7dc8cd0d83
ryaneise <67476045+ryaneise@users.noreply.github.com>
2026-08-01 23:07:21 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,5 @@
+document.getElementById("sceneText").innerHTML =
+    "<h2>GAME.JS IS WORKING</h2><p>The JavaScript file loaded successfully.</p>";
+
+document.getElementById("choices").innerHTML =
+    "<button onclick='alert(\"Button works!\")'>TEST BUTTON</button>";
